baf65eb0742039a2d76e3db2e9cb9429ec59df00
Tony Blair Maina <blairpeters13@gmail.com>
2024-09-03 11:51:19 +0300

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     list = []
+#     print(list)
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        a, b = 0, 1
+        result = [a]
+        while len(result) < length:
+            a, b = b, a + b
+            result.append(a)
+        print(result)
+
+    
